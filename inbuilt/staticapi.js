74ece470290c248b568bfeb8e62f131b775abc1e
Soubhagya Ranjan Nayak <soubhagyaranjannayak68@gmail.com>
2024-02-16 21:07:07 +0530

    creating static api

---
+++ inbuilt/staticapi.js
@@ -0,0 +1,12 @@
+let fs=require('fs');
+let http=require('http');
+
+let server=http.createServer(function(req,res){
+    fs.readFile('city.json','utf-8',function(err,data){
+        if(err) throw err;
+        //return data of file
+        res.write(data);
+        res.end();
+    })
+})
+server.listen(7860);
