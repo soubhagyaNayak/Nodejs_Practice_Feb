4506fdf47365e3b5f0e155b56cc9cea3c8345095
Soubhagya Ranjan Nayak <soubhagyaranjannayak68@gmail.com>
2024-02-19 11:35:13 +0530

    create a server using the static api and read the file using fs.readfile

--- inbuilt/staticapi.js
+++ inbuilt/staticapi.js
@@ -2,6 +2,7 @@ let fs=require('fs');
 let http=require('http');
 
 let server=http.createServer(function(req,res){
+    //read file with fs
     fs.readFile('city.json','utf-8',function(err,data){
         if(err) throw err;
         //return data of file
