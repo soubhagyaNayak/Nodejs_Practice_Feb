15ef9eec16ed721c444a7e480fdb1e7d9091a94b
Soubhagya Ranjan Nayak <soubhagyaranjannayak68@gmail.com>
2024-03-03 22:01:58 +0530

    done

--- practice/app.js
+++ practice/app.js
@@ -3,14 +3,18 @@ let dotenv=require('dotenv');
 dotenv.config();
 let port=process.env.PORT || 5689;
 let app=express();
+let categoryRouter=require('./src/controller/categoryRouter');
+let productRouter=require('./src/controller/productRouter');
+
 app.get('/',(req,res)=>{
-    res.send('Hii express');
+    res.send('Hii babuni');
 });
-app.get('/test',(req,res)=>{
-    res.send('Test route');
-});
+
+app.use('/category',categoryRouter);
+app.use('/products',productRouter);
+
 app.listen(port,(err)=>{
     if (err) throw err;
     else
-    console.log(`server listening successfully on port ${port}`)
-})
+    console.log(`server listening successfully on port ${port}`);
+});
